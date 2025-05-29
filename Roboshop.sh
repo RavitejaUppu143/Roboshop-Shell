9c5b4a39ab073c80d5d5365799f986a99d044913
RavitejaUppu143 <Ravitejauppu05061998@gmail.com>
2025-05-29 15:28:16 +0530

    Creating instances and route53 records

--- Roboshop.sh
+++ Roboshop.sh
@@ -11,7 +11,7 @@ DOMAIN_NAME="ravitejauppu.site"
 
 for instance in ${INSTANCES[@]}
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-040370d16c0a7a644 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test}]" --query "Instances[0].InstanceId"  --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-040370d16c0a7a644 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId"  --output text)
 if [ $instance != "frontend" ]
 then 
  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)

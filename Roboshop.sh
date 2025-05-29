4b7f6d4dc4868fa4aaacbf2ca745df05883c42dc
RavitejaUppu143 <Ravitejauppu05061998@gmail.com>
2025-05-29 15:19:20 +0530

    Creating instances and route53 records

--- Roboshop.sh
+++ Roboshop.sh
@@ -13,14 +13,13 @@ for instance in ${INSTANCES[@]}
 do
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-040370d16c0a7a644 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test}]" --query "Instances[0].InstanceId"  --output text)
 if( $instance != "frontend" )
- then 
-     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PrivateIpAddress" --output text)
-     RECORD_NAME="$instance.$DOMAIN_NAME"
- else
-     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PublicIpAddress" --output text)
-fi
-then
-
+then 
+ IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+ RECORD_NAME="$instance.$DOMAIN_NAME"
+else
+IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+RECORD_NAME="$DOMAIN_NAME"
+fi 
 aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
@@ -38,3 +37,4 @@ aws route53 change-resource-record-sets \
         }
         }]
     }'
+done

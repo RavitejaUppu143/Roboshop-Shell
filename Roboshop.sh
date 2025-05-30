8f296775046d37e18c9beb6e1ad4be59915baa0d
RavitejaUppu143 <Ravitejauppu05061998@gmail.com>
2025-05-31 00:32:40 +0530

    Roboshop file is updated

--- Roboshop.sh
+++ Roboshop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#!/usr/local/bin/aws
+
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-040370d16c0a7a644"
@@ -12,13 +12,13 @@ DOMAIN_NAME="ravitejauppu.site"
 
 for instance in $@
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-040370d16c0a7a644 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId"  --output text)
+INSTANCE_ID=$(/usr/local/aws-cli/v2/current/bin/aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-040370d16c0a7a644 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId"  --output text)
 if [ $instance != "frontend" ]
 then 
- IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+ IP=$(/usr/local/aws-cli/v2/current/bin/aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
  RECORD_NAME="$instance.$DOMAIN_NAME"
 else
-IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+IP=$(/usr/local/aws-cli/v2/current/bin/aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 RECORD_NAME="$DOMAIN_NAME"
 fi 
 aws route53 change-resource-record-sets \
